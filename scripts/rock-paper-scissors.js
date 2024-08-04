d7b7f3315c521f27a94e4edb850c8ea3bb17f63b
jacobo <wdjacobo@gmail.com>
2024-08-04 20:46:56 +0200

    Add logic to play a single round

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -1,3 +1,9 @@
+let computerScore = 0;
+let humanScore = 0;
+let roundNumber = 1;
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
 function getComputerChoice() {
   let shape;
   let value = Math.floor(Math.random() * 3);
@@ -19,7 +25,61 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let shape = prompt('Write your choice: "rock", "paper" or "scissors"');
-    return shape;
+  let shape = prompt(
+    "Round #" +
+      roundNumber +
+      '\nWrite your choice: "rock", "paper" or "scissors"'
+  );
+  return shape.toLowerCase();
 }
 
+function playRound(computerChoice, humanChoice) {
+  console.log("The computer chose: " + computerChoice);
+  console.log("You chose " + humanChoice);
+
+  if (computerChoice === humanChoice) {
+    console.log("It's a draw!");
+  } else {
+    switch (computerChoice) {
+      case "rock":
+        if (humanChoice === "paper") {
+          console.log("You win! Paper beats rock");
+          humanScore++;
+        } else if (humanChoice === "scissors") {
+          console.log("You lose! Rock beats scissors");
+          computerScore++;
+        }
+        break;
+      case "paper":
+        if (humanChoice === "rock") {
+          console.log("You lose! Paper beats rock");
+          computerScore++;
+        } else if (humanChoice === "scissors") {
+          console.log("You win! Scissors beats paper");
+          humanScore++;
+        }
+        break;
+      case "scissors":
+        if (humanChoice === "rock") {
+          console.log("You win! Rock beats scissors");
+          humanScore++;
+        } else if (humanChoice === "paper") {
+          console.log("You lose! Scissors beats paper");
+          computerScore++;
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  console.log(
+    "\nSCORES:\nComputer score: " +
+      computerScore +
+      "\nYour score " +
+      humanScore +
+      "\n\n"
+  );
+}
+
+playRound(computerSelection, humanSelection);
